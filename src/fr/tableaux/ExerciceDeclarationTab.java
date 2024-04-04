0eeea3d13a910a357cd8428119566ce0c2d4392e
justinergs <justineragues@gmail.com>
2024-04-04 15:18:11 +0200

    Tp 5

---
+++ src/fr/tableaux/ExerciceDeclarationTab.java
@@ -0,0 +1,20 @@
+package fr.tableaux;
+
+public class ExerciceDeclarationTab {
+
+	public static void main(String[] args) {
+	      int[] array = {8, -7, 12, 1, -2, 14, 17, 9};
+
+	        System.out.println("Index 0 : " + array[0]);
+
+	        System.out.println("Longueur du tableau : " + array.length);
+
+	        int dernierIndex = array.length - 1;
+	        System.out.println("Dernière position : " + array[dernierIndex]);
+
+	        int index = 10;
+	        System.out.println("Index 10 : " + array[index]);
+	        // Index est en dehors de la limite du tableau (8 index)
+	}
+
+}
---
+++ src/fr/tableaux/ExerciceTableauChaines.java
@@ -0,0 +1,23 @@
+package fr.tableaux;
+
+public class ExerciceTableauChaines {
+
+	public static void main(String[] args) {
+		String[] villes = {"Montpellier", "Paris", "Marseille", "Toulouse", "Béziers"};
+
+        System.out.println("Les 5 éléments du tableau :");
+        for (String ville : villes) {
+            System.out.println(ville);
+        }
+        
+        System.out.println("La longueur du tableau : " + villes.length);
+
+        
+        villes[3] = "Reims";
+
+        System.out.println("Les 5 éléments du tableau après modification de l'index 3 :");
+        for (String ville : villes) {
+            System.out.println(ville);
+        }
+	}
+}
